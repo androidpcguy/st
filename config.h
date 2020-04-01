14b0e64d1dbf915161710a6d2a50344d37477bfc
Akshara Balachandra <akshara.bala.18@gmail.com>
2020-03-31 20:12:43 -0400

    changed font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
+static char *font = "Fira Code:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { "Noto Color Emoji:pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
